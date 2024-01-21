ddff8d33a89933267623cb20832101ea5f291db2
Sushanth Reddy <sushanthreddy717@gmail.com>
2024-01-21 16:40:34 +0530

    Add files via upload

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center"> 
+          Food Delivery Website - 2023-2025, All Rights Reserved</p>
+        </footer>
+    </>
+  );
+}
+
+export default Footer
