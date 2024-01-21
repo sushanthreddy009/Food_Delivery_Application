b2ab50e0ea1e3e10c7e1673092118fc3ce610dc9
Sushanth Reddy <sushanthreddy717@gmail.com>
2024-01-21 16:50:12 +0530

    Add files via upload

---
+++ frontend/src/constants/restaurantConstant.js
@@ -0,0 +1,11 @@
+// IN this file will define action Types
+
+export const ALL_RESTAURANTS_REQUEST = "ALL_RESTAURANTS_REQUEST";
+export const ALL_RESTAURANTS_SUCCESS = "ALL_RESTAURANTS_SUCCESS";
+export const ALL_RESTAURANTS_FAIL = "ALL_RESTAURANTS_FAIL";
+
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+export const SORT_BY_RATINGS = "SORT_BY_RATINGS";
+export const SORT_BY_REVIEWS = "SORT_BY_REVIEWS";
+export const TOGGLE_VEG_ONLY = "TOGGLE_VEG_ONLY";
